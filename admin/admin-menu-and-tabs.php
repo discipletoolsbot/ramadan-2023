5059c1a30810cb0a8fba52f5e64a01953681424a
corsacca <corsacca@gmail.com>
2023-03-22 14:12:04 +0100

    v1.3.2

--- admin/admin-menu-and-tabs.php
+++ admin/admin-menu-and-tabs.php
@@ -110,7 +110,7 @@ class Ramadan_2023_Tab_General {
         }
 
 
-        $prayer_fuel_ready = [ 'en_US', 'es_ES', 'es_419', 'ru_RU', 'de_DE' ];
+        $prayer_fuel_ready = [ 'en_US', 'es_ES', 'es_419', 'ru_RU', 'de_DE', 'pt_BR' ];
 
         ?>
         <table class="widefat striped">
